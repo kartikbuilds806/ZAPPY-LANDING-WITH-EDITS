d097dfa0049f07e225ea765b2781af67a13a4b2a
kartik Sharma <kartikbuilds806@gmail.com>
2026-06-03 22:00:30 +0530

    Configure Vercel Serverless OAuth for Decap CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+import decapCmsOauth from 'astro-decap-cms-oauth';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel(),
+  integrations: [decapCmsOauth()],
   trailingSlash: 'never'
 });
